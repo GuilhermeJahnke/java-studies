e966bb5c85044147662bbde0efdbac9c1a4b7374
Guilherme Jahnke Vieira <guiguijv@gmail.com>
2022-09-02 11:44:41 -0300

    standardization of repositories

--- src/domain/usecase/account/AccountUseCase.java
+++ src/domain/usecase/account/AccountUseCase.java
@@ -6,26 +6,26 @@ import domain.entities.account.AccountEntities;
 import java.util.ArrayList;
 
 public class AccountUseCase {
-    private final AccountRepository accountRepository;
+    private final AccountRepository repository;
 
     public AccountUseCase() {
-        accountRepository = new AccountRepository();
+        repository = new AccountRepository();
     }
 
     public AccountEntities create(String name, double balance) {
-        return accountRepository.createAccount(name, balance);
+        return repository.createAccount(name, balance);
     }
 
     public AccountEntities edit(String name, double balance) {
-        return accountRepository.editAccount(name, balance);
+        return repository.editAccount(name, balance);
     }
 
     public void remove(String name) {
-        accountRepository.removeAccount(name);
+        repository.removeAccount(name);
     }
 
     public ArrayList<AccountEntities> getAll() {
-        return accountRepository.getAll();
+        return repository.getAll();
     }
 
 }
--- src/domain/usecase/user/UserUseCase.java
+++ src/domain/usecase/user/UserUseCase.java
@@ -6,26 +6,26 @@ import domain.entities.User.UserEntities;
 import java.util.ArrayList;
 
 public class UserUseCase {
-    private final UserRepository userRepository;
+    private final UserRepository repository;
 
     public UserUseCase(){
-        userRepository = new UserRepository();
+        repository = new UserRepository();
     }
 
     public UserEntities create(String name, String email, String cpf, String password){
-        return userRepository.createUser(name, email, cpf, password);
+        return repository.createUser(name, email, cpf, password);
     }
 
     public UserEntities edit(String name, String email, String cpf, String password){
-        return userRepository.editUser(name, email, cpf, password);
+        return repository.editUser(name, email, cpf, password);
     }
 
     public void remove(String cpf){
-        userRepository.removeUser(cpf);
+        repository.removeUser(cpf);
     }
 
     public ArrayList<UserEntities> getAll(){
-        return userRepository.getAll();
+        return repository.getAll();
     }
 
 
