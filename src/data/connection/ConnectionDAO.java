7ba0f4cd32feca50a0445fda899c57d71c2701d3
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2022-10-19 20:32:16 -0300

    Feat/db connection DAO (#6)
    * Deleting .idea
    
    * WIP: Add Connection DAO Methods

--- src/data/connection/ConnectionDAO.java
+++ src/data/connection/ConnectionDAO.java
@@ -2,23 +2,59 @@ package data.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
   
   public class ConnectionDAO {
+    private Connection connection;
   
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //Registra o Driver
+    public ConnectionDAO() throws ClassNotFoundException {
         System.out.println("Inicio");
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-  
-        // //Abre uma conexão
-        Connection conexao = DriverManager.getConnection(
-            "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM92923", "230601");
-        
-        System.out.println("Conectado!");
-        
-        conexao.close();
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         OpenConnection();
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      }
     }
-  }
 
+    private void OpenConnection(){
+      try {
+        this.connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM92923", "230601");
+        System.out.println("Conectado!");
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+
+    public Connection GetConnection() {
+      return this.connection;
+    }
+
+    public void CloseConnection() throws SQLException {
+        this.connection.close();
+    }
+
+    public int ExecuteCommand(PreparedStatement statement) {
+      int affectedRows = 0;
+      try {
+        this.connection.setAutoCommit(false); // Entender o que é isso
+        affectedRows = statement.executeUpdate();
+        this.connection.commit();
   
+      } catch (SQLException e) {
+        System.out.println("Error to execute comand");
+        e.printStackTrace();
+        try {
+          // In case it goes wrong, it will undo the changes with the rollback
+          this.connection.rollback();
+        } catch (SQLException e1) {
+          e1.printStackTrace();
+        }
+  
+      }
+  
+      return affectedRows;
+    }
+
+  }
