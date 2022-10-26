af6614d3c2fa9ebc79c0559399177c201fa67974
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2022-10-26 19:46:43 -0300

    connecting EntryReceipt (#7)
    Co-authored-by: Gustavo <gustavo.gsilva1723@gmail.com>

--- src/data/repository/entry/EntryReceiptRepository.java
+++ src/data/repository/entry/EntryReceiptRepository.java
@@ -1,52 +1,135 @@
 package data.repository.entry;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import domain.entities.entry.EntryReceiptEntities;
+import data.connection.ConnectionDAO;
 
 import java.util.ArrayList;
 
 public class EntryReceiptRepository {
-    private final ArrayList<EntryReceiptEntities> mockDataBase = new ArrayList<>();
+    ConnectionDAO connectionManager = new ConnectionDAO();
+    
+    public EntryReceiptEntities findById(int id) {
+        PreparedStatement statement = null;
+		EntryReceiptEntities activities = null;
 
-    public EntryReceiptEntities findByDescription(String description) {
-        for (EntryReceiptEntities entryReceiptEntities : mockDataBase) {
-            if (entryReceiptEntities.getDescription().equals(description)) return entryReceiptEntities;
+        try {
+            statement = connectionManager.GetConnection().prepareStatement("SELECT * FROM T_ENTRY_RECEIPT WHERE ID_ENTRY = ?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                activities = new EntryReceiptEntities(
+                    resultSet.getBoolean("received"),
+                    resultSet.getString("receivedDate"),
+                    resultSet.getDouble("valor"),
+                    resultSet.getString("date"),
+                    resultSet.getString("category"),
+                    resultSet.getString("description")
+                );
+            }
+
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return activities;
+
+    }    
+    
+    public int createEntryReceipt(boolean received, String receivedDate, double valor, String date, String category,
+    String description) {
+        
+        int affectedRows = 0;
+        PreparedStatement statement;
+        String sql = "INSERT INTO T_ENTRY_RECEIPT (RECEBPT_RECEIVED, RECEBPT_RECIVED_DATE, RECEBPT_VALOR, RECEBPT_DATE, RECEBPT_CATEGORY, RECEBPT_DESCRIPTION) VALUES ( ?, ?, ?, ?, ?, ?)";
+
+        try {
+            statement = connectionManager.GetConnection().prepareStatement(sql);
+            statement.setBoolean(1, received);
+            statement.setString(2, receivedDate);
+            statement.setDouble(3, valor);
+            statement.setString(4, date);
+            statement.setString(5, category);
+            statement.setString(6, description);
+
+            affectedRows = connectionManager.ExecuteCommand(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }         
+
+        return affectedRows;
+    }
+
+    public int editEntryReceipt(boolean received, String receivedDate, String description,
+    double valor, String date, String category) {
+        int affectedRows = 0;
+        PreparedStatement statement;
+        String sql = "UPDATE T_ENTREY_RECEPT SET RECEBPT_RECEIVED, RECEBPT_RECIVED_DATE, RECEBPT_VALOR, RECEBPT_DATE, RECEBPT_CATEGORY, RECEBPT_DESCRIPTION = ?, balance = ?, ?, ?, ?, ?, ?";
+
+        try {
+            statement = connectionManager.GetConnection().prepareStatement(sql);
+            statement.setBoolean(1, received);
+            statement.setString(2, receivedDate);
+            statement.setDouble(3, valor);
+            statement.setString(4, date);
+            statement.setString(5, category);
+            statement.setString(6, description);
+
+            affectedRows = connectionManager.ExecuteCommand(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-        throw new IllegalArgumentException("Nenhum lançamento de receita encontrado com esta descrição");
+        return affectedRows;
     }
 
+    public int removeEntryReceipt(int id) {
+        int affectedRows = 0;
+		PreparedStatement statement;
+        String sql = "DELETE FROM T_ENTRY_RECEIPT WHERE ID_ENTRY = ?";
 
-    public EntryReceiptEntities createEntryReceipt(boolean received, String receivedDate, double valor,
-                                                   String date, String category, String description) {
+        try {
+            statement = connectionManager.GetConnection().prepareStatement(sql);
+            statement.setInt(1, id);
 
-        EntryReceiptEntities newEntryReceipt = new EntryReceiptEntities(received, receivedDate, valor, date,
-                category, description);
+            affectedRows = connectionManager.ExecuteCommand(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        mockDataBase.add(newEntryReceipt);
-
-        return newEntryReceipt;
-    }
-
-    public EntryReceiptEntities editEntryReceipt(boolean received, String receivedDate, String description,
-                                                 double valor, String date, String category) {
-
-        EntryReceiptEntities receiptEdit = findByDescription(description);
-
-        receiptEdit.setValor(valor);
-        receiptEdit.setDate(date);
-        receiptEdit.setCategory(category);
-        receiptEdit.setDescription(description);
-        receiptEdit.setReceived(received);
-        receiptEdit.setReceivedDate(receivedDate);
-
-        return receiptEdit;
-    }
-
-    public void removeEntryReceipt(String description) {
-        mockDataBase.remove(findByDescription(description));
+        return affectedRows;
     }
 
     public ArrayList<EntryReceiptEntities> getAll() {
-        return mockDataBase;
+        ArrayList<EntryReceiptEntities> activitiesList = new ArrayList<>();
+        PreparedStatement statement = null;
+		EntryReceiptEntities activities = null;
+
+        try {
+            statement = connectionManager.GetConnection().prepareStatement("SELECT * FROM T_ENTRY_RECEIPT");
+            ResultSet result = connectionManager.GetData(statement);
+
+            while (result.next()) {
+                activities = new EntryReceiptEntities(
+                    result.getBoolean("received"),
+                    result.getString("receivedDate"),
+                    result.getDouble("valor"),
+                    result.getString("date"),
+                    result.getString("category"),
+                    result.getString("description")
+                );
+                activitiesList.add(activities);
+            }
+
+            result.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return activitiesList;
     }
+
 }
